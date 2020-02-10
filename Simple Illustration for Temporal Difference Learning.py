0bf1e2260028e7892cd43557f3055018e7740b45
Hazem Essam <hazemessamm@users.noreply.github.com>
2020-02-10 04:37:32 +0200

    Update Simple Illustration for Temporal Difference Learning.py

--- Simple Illustration for Temporal Difference Learning.py
+++ Simple Illustration for Temporal Difference Learning.py
@@ -23,17 +23,19 @@ lr = 0.3
 
 j = 0
 
-for i in range(1000):
+for i in range(50):
     action = np.random.choice(policy, p = [1-environment[start], environment[start]])
     j = start
+    terminal = False
     while not terminal:
         last_action = j
         j += step(action)
         if j == 0:
             terminal = True
             environment[j+1] = environment[j+1] + lr * (0 + (0.5*environment[j]) - environment[j+1])
-        elif j == len(environment)-1:
+        elif j == 6:
             environment[j-1] = environment[j-1] + lr * (1 + (0.5*environment[j]) - environment[j-1])
+            total_rewards += 1
             terminal = True
         else:
             if j > last_action:
@@ -42,5 +44,7 @@ for i in range(1000):
                 environment[j+1] = environment[j+1] + lr * (0 + (0.5*environment[j]) - environment[j+1])
         action = np.random.choice(policy, p = [1-environment[j], environment[j]])
     if i % 100 == 0:
-        print(environment)
+        print(environment, total_rewards)
+
+
         
